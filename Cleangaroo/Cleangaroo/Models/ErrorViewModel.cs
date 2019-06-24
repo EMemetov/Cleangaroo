7003ee68a246662130103f7e8a8c6c327658edb0
Eskender Memetov <skorpi76@gmail.com>
2019-06-23 20:06:00 -0400

    .net core conversion
    Changed the project from .net framework to .net core

---
+++ Cleangaroo/Cleangaroo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cleangaroo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
