d692a04d2baff7e32192f41372dc436e2c05b83e
FERNANDO MARTINS <fernandomartins.rn@gmail.com>
2019-06-27 13:15:09 -0400

    Create a save and delete functions for userlogin

--- CleanGuruApp/CleanGuruApp/Models/DB/EFUserLoginRepository.cs
+++ CleanGuruApp/CleanGuruApp/Models/DB/EFUserLoginRepository.cs
@@ -5,7 +5,49 @@ using System.Threading.Tasks;
 
 namespace CleanGuruApp.Models.DB
 {
-    public class EFUserLoginRepository
+    public class EFUserLoginRepository : IUserLoginRepository
     {
+        private ApplicationDBContext context;
+
+        public EFUserLoginRepository(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<UserLogin> UserLogins => context.UserLogin;
+
+        public void SaveUserLogin(UserLogin userLogin)
+        {
+            //I'm not sure if we can use null or ""
+            if (userLogin.UserName.Equals(null))
+            //if (userLogin.UserName.Equals(""))
+            {
+                context.UserLogin.Add(userLogin);
+            }
+            else
+            {
+                UserLogin dbEntry = context.UserLogin
+                  .FirstOrDefault(u => u.UserName == userLogin.UserName);
+                if (dbEntry != null)
+                {
+                    dbEntry.Pin = userLogin.Pin;
+                    dbEntry.Role = userLogin.Role;
+                }
+            }
+            context.SaveChanges();
+        }
+
+
+        public void DeleteUserLogin(string userName)
+        {
+            UserLogin dbEntry = context.UserLogin
+           .FirstOrDefault(u => u.UserName == userName);
+            if (dbEntry != null)
+            {
+                context.UserLogin.Remove(dbEntry);
+                context.SaveChanges();
+            }
+
+        }
     }
 }
