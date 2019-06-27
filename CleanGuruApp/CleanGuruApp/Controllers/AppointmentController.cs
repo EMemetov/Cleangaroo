7a97d74b9d356f7c79e024424ce43e906c2a43e1
MaryStaf <mariia.staforkina@gmail.com>
2019-06-27 17:56:42 -0400

    Create Appointment (FE) - not Final

---
+++ CleanGuruApp/CleanGuruApp/Controllers/AppointmentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanGuruApp.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public ViewResult CreateAppointment()
+        {
+            return View();
+        }
+    }
+}
