d02f932f0f9ee8bb665d84acc70a8b4048283a6d
FERNANDO MARTINS <fernandomartins.rn@gmail.com>
2019-06-26 22:27:32 -0400

    Create save and delete functions for CustomerAddress. Create constraint for some fields and define primary key.

--- CleanGuruApp/CleanGuruApp/Models/CustomerAddress.cs
+++ CleanGuruApp/CleanGuruApp/Models/CustomerAddress.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,18 @@ namespace CleanGuruApp.Models
 {
     public class CustomerAddress
     {
+        [Key]
         public int IdCustAddress { get; set; }
-        public int IdCustomer { get; set; }
+        [Required(ErrorMessage = "The customer should be not blank")]
+        public int idCustomer { get; set; }
+        [Required(ErrorMessage = "The customer address should be not blank")]
         public string Address { get; set; }
         public string AddressUnit { get; set; }
+        [Required(ErrorMessage = "The postal code should be not blank")]
         public string PostalCode { get; set; }
+        [Required(ErrorMessage = "The customer city should be not blank")]
         public string City { get; set; }
+        [Required(ErrorMessage = "The customer province should be not blank")]
         public string Province { get; set; }
-        public string UserName{ get; set; }
     }
 }
