2e5d231408d1006bb9f899cff86d6660364977d1
Theo Mitchellemail <theomitchell@gmail.com>
2019-07-21 08:33:01 -0400

    Show a List of creaners

---
+++ CleanGuruApp/CleanGuruApp/Controllers/CleanerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanGuruApp.Models;
+using CleanGuruApp.Models.DB;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanGuruApp.Controllers
+{
+    public class CleanerController : Controller
+    {
+        private readonly ICleanerRepository cleanerRepository;
+
+        public CleanerController(ICleanerRepository repository)
+        {
+            cleanerRepository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Register(int idCleaner) => View(cleanerRepository.Cleaners.FirstOrDefault(c => c.IdCleaner == idCleaner));
+
+        [HttpPost]
+        public IActionResult Register(Cleaner cleaner)
+        {
+            if (ModelState.IsValid)
+            {
+                cleanerRepository.SaveCleaner(cleaner);
+                TempData["message"] = $"Cleaner {cleaner.UserName} has been successfully registered in the system";
+
+                return View("../Home/Index");
+            }
+            else
+            {
+                return View(cleaner);
+            }
+        }
+
+        public async Task<IActionResult> List()
+        {
+            //return View(await cleanerRepository.Cleaner.ToListAsync());
+            return View();
+        }
+    }
+}
